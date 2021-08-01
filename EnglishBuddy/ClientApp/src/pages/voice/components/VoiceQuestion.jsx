d66508edb512a0bca76fc8fb3d11360b2894a0a2
HCRJeewantha <ravindujxyz@gmail.com>
2021-08-01 18:13:02 +0530

    ui changes

--- EnglishBuddy/ClientApp/src/pages/voice/components/VoiceQuestion.jsx
+++ EnglishBuddy/ClientApp/src/pages/voice/components/VoiceQuestion.jsx
@@ -1,12 +1,9 @@
 export default function VoiceQuestion(props) {
   return (
     <div className="grid grid-cols-8 gap-6">
-      <div className="shadow-sm col-start-2 col-span-6">
-        <h3><b>{props.title}</b></h3>
-        <br/>
-        <div>
-          <br/>
-        </div>
+      <div className="shadow-sm col-start-2 col-span-6 grid justify-items-center text-lg">
+        <b>{props.title}</b>
+        <br />
       </div>
     </div>
   )
