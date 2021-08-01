76d177ae34bfde784934fd1331f277a61b9f81b0
HCRJeewantha <ravindujxyz@gmail.com>
2021-08-01 17:42:03 +0530

    title changed

--- EnglishBuddy/ClientApp/src/pages/voice/components/VoiceQuestion.jsx
+++ EnglishBuddy/ClientApp/src/pages/voice/components/VoiceQuestion.jsx
@@ -2,11 +2,9 @@ export default function VoiceQuestion(props) {
   return (
     <div className="grid grid-cols-8 gap-6">
       <div className="shadow-sm col-start-2 col-span-6">
-
-        <b>{props.title}</b>
+        <h3><b>{props.title}</b></h3>
         <br/>
         <div>
-          <br/>
           <br/>
         </div>
       </div>
