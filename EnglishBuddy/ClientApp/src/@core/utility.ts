95c3351c7cf94ba612757642b127ad36bad6eac4
HCRJeewantha <ravindujxyz@gmail.com>
2021-08-03 02:27:20 +0530

    ui home

--- EnglishBuddy/ClientApp/src/@core/utility.ts
+++ EnglishBuddy/ClientApp/src/@core/utility.ts
@@ -1,18 +1,9 @@
 export class Utility {
   public static BASE_URL = 'https://english-buddy-365.herokuapp.com/api/';
-  // public static BASE_URL = 'https://localhost:5001/api/';
-
   public static SIMILARITY_API_URL = 'https://semantic-similarity-api.herokuapp.com';
-  // public static SIMILARITY_API_URL = 'http://localhost:8000';
-
   public static ESSAY_API_URL = 'https://essay-evaluation-api.herokuapp.com';
-  // public static ESSAY_API_URL = 'http://localhost:8001';
-
   public static AUDIO_TEXT_TRANSCRIPTION_API_URL = 'https://english-buddy-speech.herokuapp.com';
-  // public static AUDIO_TEXT_TRANSCRIPTION_API_URL = 'http://localhost:8000';
-
   public static EMPTY = '';
-
   public static PARAPHRASING = 'Paraphrasing';
   public static SUMMARIZATION = 'Summarization';
   public static ESSAY = 'Essay';
