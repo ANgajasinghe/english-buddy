9c67a338d3551f140b9f4a694fcf125d93806c63
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-08-03 23:28:33 +0530

    dashboard

--- EnglishBuddy/ClientApp/src/pages/my-courses/my-course-details/components/IntroductionLesson.tsx
+++ EnglishBuddy/ClientApp/src/pages/my-courses/my-course-details/components/IntroductionLesson.tsx
@@ -55,7 +55,6 @@ export default function IntroductionLesson(props: {
             onClick={() => setOpen(true)}>
             Are you stuck?
           </button>
-
           <div className="flex absolute left-0 h-4 w-4">
             <span className="animate-ping  absolute inline-flex h-4 w-4 rounded-full bg-red-400 opacity-75"/>
             <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"/>
@@ -64,6 +63,27 @@ export default function IntroductionLesson(props: {
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-3 w-3 bg-purple-500"></span>
         </span> */}
+        </div>
+
+        <div className="absolute flex right-0 top-32">
+          <button
+            className="border-r-0
+        border-l-2 border-b-2 border-t-2
+        rounded-l-full
+        border-red-500
+        font-bold
+        text-red-500
+        text-sm
+        relative
+        p-2
+        px-3
+        transition
+        duration-300
+        ease-in-out
+        hover:bg-red-500
+        hover:text-white">
+            Course history
+          </button>
         </div>
 
         <IntroDialogBox
