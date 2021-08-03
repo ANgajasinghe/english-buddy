8c4feb87a5469b07bf8e725b7195ffde6ccc92ef
HCRJeewantha <ravindujxyz@gmail.com>
2021-08-03 22:47:47 +0530

    ui changes

--- EnglishBuddy/ClientApp/src/pages/voice/components/Assistant.jsx
+++ EnglishBuddy/ClientApp/src/pages/voice/components/Assistant.jsx
@@ -7,11 +7,11 @@ export default function Assistant(props) {
   return (
     <div className="py-1 px-3 ">
       <div className="grid grid-cols-3 gap-1 px-4 rounded-lg bg-opacity-31">
-          <div className=" font-bold text-lg">{props.description}</div>
+          <div className=" font-bold text-lg">{ReactHtmlParser(props.description)}</div>
       </div>
       <div className="mt-5 px-4  rounded-lg bg-opacity-31">
         <div className="text-lg font-semibold py-2">Hints</div>
-        <div className="text-lg">{ReactHtmlParser(props.modelAnswer)}</div>
+        <div className="text-lg">{props.modelAnswer}</div>
       </div>
     </div>
   );
