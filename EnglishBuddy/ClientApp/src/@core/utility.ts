d7726d394e865afbd6ea808bba7133ff6b955925
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-12 23:06:18 +0530

    fix urls

--- EnglishBuddy/ClientApp/src/@core/utility.ts
+++ EnglishBuddy/ClientApp/src/@core/utility.ts
@@ -1,9 +1,9 @@
 export class Utility {
-    // public static BASE_URL = 'https://english-buddy-365.herokuapp.com/api/';
-    public static BASE_URL = 'https://localhost:5001/api/';
+    public static BASE_URL = 'https://english-buddy-365.herokuapp.com/api/';
+    // public static BASE_URL = 'https://localhost:5001/api/';
 
-    //public static SIGNALR = 'https://english-buddy-365.herokuapp.com/';
-    public static SIGNALR = 'https://localhost:5001/';
+    public static SIGNALR = 'https://english-buddy-365.herokuapp.com/';
+    // public static SIGNALR = 'https://localhost:5001/';
 
     public static SIMILARITY_API_URL = 'https://semantic-similarity-api.herokuapp.com';
     public static ESSAY_API_URL = 'https://essay-evaluation-api.herokuapp.com';
